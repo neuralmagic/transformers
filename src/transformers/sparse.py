756a9bbbf3c6193af7101b5db54c0d962b4d51bb
Eldar Kurtic <eldar.ciki@gmail.com>
2022-01-24 20:18:37 +0100

    Fix incorrect steps calculation when gradient acc. (#31)
    When gradient accumulation is used, the effective batch size is `gradent_accumulation_steps` times larger.

--- src/transformers/sparse.py
+++ src/transformers/sparse.py
@@ -96,9 +96,8 @@ class SparseMLTrainer(Trainer):
         super().create_optimizer()
         if not self.recipes:
             return
-        steps_per_epoch = math.ceil(
-            len(self.train_dataset) / (self.args.per_device_train_batch_size * self.args._n_gpu)
-        )
+        total_batch_size = self.args.per_device_train_batch_size * self.args._n_gpu * self.args.gradient_accumulation_steps
+        steps_per_epoch = math.ceil(len(self.train_dataset) / total_batch_size)
         self.args.num_train_epochs = float(self.manager.max_epochs)
         if hasattr(self, "scaler"):
             self.scaler = self.manager.modify(
